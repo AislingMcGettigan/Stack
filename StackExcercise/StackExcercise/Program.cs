aac3c4f4c6970e022f8e6d2e177424e29d3d18d2
aisling mcgettigan <aislingmcgettigan93@hotmail.com>
2020-02-21 18:09:51 -0800

    Stack excercise, add objects to the stack , remove objects from the stack

---
+++ StackExcercise/StackExcercise/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackExcercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stack = new Stack();
+            int max = 5;
+
+            for(var i = 0; i <= max; i++)
+            {
+                stack.Push(i);
+            }
+
+            for (var i =1; i <= max; i++)
+            {
+                Console.WriteLine(stack.Pop());
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
